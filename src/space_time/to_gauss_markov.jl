11fecbcf048ceb6a56de52fdea64be7662458ea0
willtebbutt <wt0881@my.bristol.ac.uk>
2020-05-11 01:50:42 +0100

    Removes redundant code

--- src/space_time/to_gauss_markov.jl
+++ src/space_time/to_gauss_markov.jl
@@ -14,28 +14,16 @@ function GaussMarkovModel(k::Separable, x::SpaceTimeGrid, storage)
     # Produce a new GaussMarkovModel over the spatial locations specified.
     Nr = length(r)
     ident = my_I(eltype(storage), Nr)
-    # A = kron.(Ref(Eye(Nr)), gmm_time.A)
     A = map(A -> kron(ident, A), gmm_time.A)
     a = map(a -> repeat(a, Nr), gmm_time.a)
-    # a = repeat.(gmm_time.a, Nr)
     Q = map(Q -> kron(Kr, Q), gmm_time.Q)
     H = map(H -> kron(ident, H), gmm_time.H)
-    # H = build_H(ident, gmm_time.H)
-    # h = repeat.(gmm_time.h, Nr)
     h = map(h -> repeat(h, Nr), gmm_time.h)
     x = Gaussian(
         repeat(gmm_time.x0.m, Nr),
         kron(Kr, gmm_time.x0.P),
     )
     return GaussMarkovModel(A, a, Q, H, h, x)
-end
-
-function build_H(ident, Hs::Fill)
-    return Fill(kron(ident, Hs.value), length(Hs))
-end
-
-function build_H(ident, Hs::AbstractVector)
-    return kron.(Ref(ident), gmm_time.H)
 end
 
 function (f::LTISDE)(x::SpaceTimeGrid, Σs::AV{<:AM{<:Real}})
