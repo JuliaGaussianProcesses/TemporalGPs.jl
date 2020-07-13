a68ff3efecb541343fe4ede09bc7006f2be65c1a
willtebbutt <wt0881@my.bristol.ac.uk>
2020-07-13 21:01:15 +0100

    Removed Strided dependency

--- src/TemporalGPs.jl
+++ src/TemporalGPs.jl
@@ -1,7 +1,7 @@
 module TemporalGPs
 
     using BlockDiagonals, FillArrays, Kronecker, LinearAlgebra, Random,
-        StaticArrays, Stheno, Strided, Zygote, ZygoteRules
+        StaticArrays, Stheno, Zygote, ZygoteRules
 
     using FillArrays: AbstractFill
     using Kronecker: KroneckerProduct
