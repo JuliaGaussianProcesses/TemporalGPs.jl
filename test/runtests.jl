500ed23b4759fefe27dfb3a0d0a68cc011814cf1
Will Tebbutt <wt0881@my.bristol.ac.uk>
2020-11-30 15:38:07 +0000

    Fix doctests usage.

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,52 +17,52 @@ include("test_util.jl")
 
 @testset "TemporalGPs.jl" begin
 
-    # println("util:")
-    # @testset "util" begin
-    #     include(joinpath("util", "zygote_rules.jl"))
-    #     include(joinpath("util", "gaussian.jl"))
-    #     include(joinpath("util", "mul.jl"))
-    #     include(joinpath("util", "regular_data.jl"))
-    # end
+    println("util:")
+    @testset "util" begin
+        include(joinpath("util", "zygote_rules.jl"))
+        include(joinpath("util", "gaussian.jl"))
+        include(joinpath("util", "mul.jl"))
+        include(joinpath("util", "regular_data.jl"))
+    end
 
-    # include(joinpath("models", "model_test_utils.jl"))
-    # include(joinpath("models", "test_model_test_utils.jl"))
-    # @testset "models" begin
-    #     include(joinpath("models", "gauss_markov.jl"))
-    #     include(joinpath("models", "lgssm.jl"))
+    include(joinpath("models", "model_test_utils.jl"))
+    include(joinpath("models", "test_model_test_utils.jl"))
+    @testset "models" begin
+        include(joinpath("models", "gauss_markov.jl"))
+        include(joinpath("models", "lgssm.jl"))
 
-    #     include(joinpath("models", "immutable_inference.jl"))
-    #     include(joinpath("models", "immutable_inference_pullbacks.jl"))
-    #     include(joinpath("models", "checkpointed_immutable_pullbacks.jl"))
+        include(joinpath("models", "immutable_inference.jl"))
+        include(joinpath("models", "immutable_inference_pullbacks.jl"))
+        include(joinpath("models", "checkpointed_immutable_pullbacks.jl"))
 
-    #     include(joinpath("models", "mutable_inference.jl"))
-    #     include(joinpath("models", "mutable_inference_pullbacks.jl"))
+        include(joinpath("models", "mutable_inference.jl"))
+        include(joinpath("models", "mutable_inference_pullbacks.jl"))
 
-    #     include(joinpath("models", "scalar_lgssm.jl"))
-    # end
+        include(joinpath("models", "scalar_lgssm.jl"))
+    end
 
-    # @testset "gp" begin
-    #     include(joinpath("gp", "to_gauss_markov.jl"))
-    #     include(joinpath("gp", "lti_sde.jl"))
-    #     include(joinpath("gp", "finite_lti_sde.jl"))
-    #     include(joinpath("gp", "posterior_lti_sde.jl"))
-    # end
+    @testset "gp" begin
+        include(joinpath("gp", "to_gauss_markov.jl"))
+        include(joinpath("gp", "lti_sde.jl"))
+        include(joinpath("gp", "finite_lti_sde.jl"))
+        include(joinpath("gp", "posterior_lti_sde.jl"))
+    end
 
-    # println("space_time:")
-    # @testset "space_time" begin
-    #     include(joinpath("space_time", "rectilinear_grid.jl"))
-    #     include(joinpath("space_time", "separable_kernel.jl"))
-    #     include(joinpath("space_time", "to_gauss_markov.jl"))
-    # end
+    println("space_time:")
+    @testset "space_time" begin
+        include(joinpath("space_time", "rectilinear_grid.jl"))
+        include(joinpath("space_time", "separable_kernel.jl"))
+        include(joinpath("space_time", "to_gauss_markov.jl"))
+    end
 
     println("doctests")
-    @timedtestset "doctests" begin
+    @testset "doctests" begin
         DocMeta.setdocmeta!(
-            Stheno,
+            TemporalGPs,
             :DocTestSetup,
-            :(using Stheno, Random, Documenter, LinearAlgebra);
+            :(using Documenter, LinearAlgebra, Random, TemporalGPs);
             recursive=true,
         )
-        doctest(Stheno)
+        doctest(TemporalGPs)
     end
 end
