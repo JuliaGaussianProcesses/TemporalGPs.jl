ee09345abd1ad68e3a3153aabed6c3e285b1d165
WT <wt0881@my.bristol.ac.uk>
2021-04-06 20:06:54 +0100

    Add reversion test

--- test/util/gaussian.jl
+++ test/util/gaussian.jl
@@ -11,4 +11,9 @@ end
 @testset "Gaussian" begin
     N = 11
     @test TemporalGPs.dim(Gaussian(randn(N), randn(N, N))) == N
+
+    @testset "Reversion test for #56" begin
+        x = Gaussian(SVector{3}(randn(3)), zeros(3, 3))
+        @test rand(x) ≈ mean(x) rtol=1e-6
+    end
 end
